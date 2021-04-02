6255424dbf027409cf7af04f28ffedebe3bc2a5d
Phil Schaffer <phil.schaffer@dmcinfo.com>
2021-04-02 10:27:54 -0700

    reindex

--- app/main.py
+++ app/main.py
@@ -3,14 +3,19 @@ import os
 
 app = Flask(__name__)
 
+FOLDER = os.path.join('static')
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = FOLDER
+
 @app.route('/index')
 def show_index():
-    #full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
-    return render_template("index.html", user_image = 'static/winner.jpg')
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'winner.jpg')
+    return render_template("index.html", user_image = full_filename)
 
 @app.route('/')
 def hello_world():
-  return 'Hi Lillian, I have a website! from, Phil'
+  return 'Hi Lillian, I have a website! from, Phil!'
 
 if __name__ == '__main__':
   app.run()
