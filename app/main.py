4487c898fdf958ae2392c960b805b3e3f41a4063
Phil Schaffer <phil.schaffer@dmcinfo.com>
2021-04-02 09:10:15 -0700

    add index

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
+
+@app.route('/index')
+def show_index():
+    #full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
+    return render_template("index.html", user_image = 'static/winner.jpg')
 
 @app.route('/')
 def hello_world():
